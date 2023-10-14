0c1126efd05b3e9c6a88cc1e6f16de1ebbf9c279
Robert Bikmurzin <bikmurzinrm@gmail.com>
2023-10-14 17:27:26 +0400

    Created Empty Project

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,23 @@
+////   /*
+//
+//  Project: RickAndMorty
+//  File: ViewController.swift
+//  Created by: Robert Bikmurzin
+//  Date: 14.10.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
